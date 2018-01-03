fab2fbaee6cd944abd5af478916faf248bf6fbcb
Steven Senkowski <scsenkowski@gmail.com>
2018-01-03 00:41:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,14 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    now = Time.now
+    morning = time.hour <= 11
+    afternoon = time.hour >= 12
 
+    if morning == true
+      resp.write
+    else
+
+    end
     resp.finish
   end
